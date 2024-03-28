68dbb400800d7c59f5bf9195701085f668401b98
el-buku <56129429+el-buku@users.noreply.github.com>
2024-03-28 05:27:46 +0200

    summary download file decoders and more

--- src/utils/fileEncoder.js
+++ src/utils/fileEncoder.js
@@ -12,3 +12,38 @@ export const downloadBase64File = (data, fileName) => {
   downloadLink.download = fileName;
   downloadLink.click();
 };
+
+export function escapeWinAnsi(text) {
+  return text.replace(/[\u0080-\uffff]/g, (char) => '');
+}
+
+export const base64toFile = async ({ data, name }) => {
+  const ext = name.split('.').pop();
+  const contentType = data.split(';')[0].split(':')[1];
+  const response = await fetch(data);
+  const buff = await response.arrayBuffer();
+  const blob = new Blob([buff], { type: contentType });
+
+  return { data, name, blob, buff, file: new File([buff], name, { type: contentType }), ext };
+
+  // const data = b64F.split(';');
+  // const contentType = data[0];
+  // const b64Data = data[1].split('base64,')[1];
+  // console.log({ contentType, b64Data });
+  // const byteCharacters = atob(b64Data);
+  // const byteArrays = [];
+
+  // for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+  //   const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+  //   const byteNumbers = new Array(slice.length);
+  //   for (let i = 0; i < slice.length; i++) {
+  //     byteNumbers[i] = slice.charCodeAt(i);
+  //   }
+
+  //   const byteArray = new Uint8Array(byteNumbers);
+  //   byteArrays.push(byteArray);
+  // }
+
+  // return blob;
+};
--- src/views/pipeline/Summary.js
+++ src/views/pipeline/Summary.js
@@ -3,26 +3,57 @@ import { QUOTE_BASE_STATUS, QUOTE_PIPELINE_STATUS, QUOTE_TYPE } from 'src/contex
 import { isScheduledQuote } from 'src/context/Quotes/selectors';
 import BaseQuotesList from '../../components/shared/quotes/base/List';
 import AddQuoteModal from '../../components/shared/quotes/base/Modal';
-import { Button } from '@mui/material';
-import React from 'react';
+import { Button, IconButton } from '@mui/material';
+import React, { useState } from 'react';
 import DashboardCard from '../../components/shared/DashboardCard';
 import { QUOTE_STATUS } from 'src/context/Quotes/defs';
 import { isBaseQuote } from 'src/context/Quotes/selectors';
 import { isApprovedNotCompletedQuote } from 'src/context/Quotes/selectors';
+import { CircularProgress } from '@mui/material';
+import { mergeQuotesAndFilesPDF } from 'src/components/shared/quotes/quotesDownload/mergePdf';
+
+function triggerDownload(pdfBytes) {
+  const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'quotes-summary.pdf';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
 const Summary = () => {
   const { quotes, editQuote } = useQuotesContext();
-  const products = quotes.filter((q) => q.status != QUOTE_BASE_STATUS.UNAPPROVED);
-  //   console.log(products);
-  //   const handleCompleted = (q) => {
-  //     editQuote(q.id, { ...q, status: QUOTE_PIPELINE_STATUS.COMPLETE_NOT_PAID });
-  //   };
-  //   const handleStarted = (q) => {
-  //     editQuote(q.id, { ...q, status: QUOTE_PIPELINE_STATUS.SCHEDULED_STARTED });
-  //   };
+  const approvedQuotes = quotes.filter((q) => q.status != QUOTE_BASE_STATUS.UNAPPROVED);
+
+  //   const [preparedSummary, setPreparedSummary] = useState();
+  const [isPreparing, setIsPreparing] = useState(false);
+  const startPreparingDownload = async () => {
+    setIsPreparing(true);
+    const pdfBytes = await mergeQuotesAndFilesPDF(approvedQuotes);
+    // setPreparedSummary(pdfBytes)
+    triggerDownload(pdfBytes);
+    setIsPreparing(false);
+  };
   return (
     <>
       <BaseQuotesList
-        quotes={products}
+        quotes={approvedQuotes}
+        mainButton={
+          <Button onClick={startPreparingDownload} variant="contained">
+            {isPreparing ? (
+              <>
+                {'Preparing...'} <CircularProgress size={20} color="text" sx={{ ml: 2 }} />
+              </>
+            ) : (
+              'Download Summary'
+            )}
+          </Button>
+          // <IconButton color="info" variant="contained">
+          //   <IconFidgetSpinner /> Preparing ...
+          // </IconButton>
+        }
         title={'Quotes Summary'}
         renderFields={['quote_type']}
         actionBtnRenderer={(q) => {
